c66e608c9e91d10c4d3e2caf2048380f46a3d44d
Lars <lars.barnekow@gmx.de>
2021-09-03 09:39:46 +0200

    Fix

--- chatbot/src/components/App.jsx
+++ chatbot/src/components/App.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import Signup from "./Signup";
 import { MainPage } from "./MainPage";
-import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App(){
     return(
-        <Container>
-                <Router>
-                    <AuthProvider>
-                        <Switch>
-                            <Route path="/signup" component={Signup} />
-                            <Route path="/" component={MainPage} />
-                        </Switch>
-                    </AuthProvider>
-                </Router>
-        </Container>
+        <Router>
+            <AuthProvider>
+                <Switch>
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/" component={MainPage} />
+                </Switch>
+            </AuthProvider>
+        </Router>
     )
 }
 
