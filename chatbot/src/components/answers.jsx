a75f7e201a9482e2f3b31de4fef63816369ff75f
shiva <shivaahuja001@gmail.com>
2021-09-13 11:42:41 +0200

    Questions and Answers

--- chatbot/src/components/answers.jsx
+++ chatbot/src/components/answers.jsx
@@ -11,6 +11,25 @@ const answerArray = [
 		question: "Lorem",
 		answer: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
 	},
+	{
+		question: "How do I change my windows password?",
+		answers:"Select Start > Settings > Accounts > Sign-in options . Under Password, select the Change button"
+	},
+	{
+		question: "What is the customer support number?",
+		answers: "The customer support number is : 0171 1234123. We are available for you on weekdays from 8 am to 8 pm. "
+	},
+	{
+		question: "Help",
+		answers: "Customer support number : 0171 1234123. Customer support Email Address: wehelpyou@itech.com "
+	},
+	{
+		question: "How can I download drivers for my printer ?",
+		answers: "Go to https://support.hp.com/de-de/drivers/printers , enter your printer product number, click on download."
+	},
+	{
+		
+	}
 ];
 
 export const answers = answerArray;
