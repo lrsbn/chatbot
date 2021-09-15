8424b9198e1037b2b5bfa2732ff1cb12523d7261
Lars Barnekow <lars.barnekow@gmx.de>
2021-09-15 08:06:36 +0200

    Small Tweaks

--- chatbot/src/components/Login.jsx
+++ chatbot/src/components/Login.jsx
@@ -22,7 +22,7 @@ export const Login = () => {
             }).catch(error => {
                 setError(error.code.replace("auth/", "").replace("-", " "));
             }).finally(() => {
-                setLoading(false)
+                setLoading(false);
             })
         }
     }
--- chatbot/src/components/Signup.jsx
+++ chatbot/src/components/Signup.jsx
@@ -1,4 +1,3 @@
-
 import React, {useRef, useState} from 'react'
 import { NavLink, useHistory } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
